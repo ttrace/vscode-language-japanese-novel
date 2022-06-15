9644b74401ea0df97649f611ea913dc78585ab79
Taiyo Fujii <taiyo@taiyolab.com>
2022-06-15 09:29:57 +0900

    releasing 1.3.3 品詞ハイライトのオンオフを実装

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -40,8 +40,14 @@ export function activateTokenizer(context: vscode.ExtensionContext, kuromojiPath
 	tokenizeFlag = true;
 }
 
-export function desableTokenizer(){
+export function desableTokenizer() {
 	tokenizeFlag = false;
+	vscode.languages.registerDocumentSemanticTokensProvider({ language: 'novel' }, new DocumentSemanticTokensProvider(), legend);
+}
+
+export function enableTokenizer() {
+	tokenizeFlag = true;
+	vscode.languages.registerDocumentSemanticTokensProvider({ language: 'novel' }, new DocumentSemanticTokensProvider(), legend);
 }
 
 interface IParsedToken {
@@ -57,158 +63,164 @@ export class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTo
 	async provideDocumentSemanticTokens(document: vscode.TextDocument, token: vscode.CancellationToken): Promise<vscode.SemanticTokens> {
 		//const allTokens = this._parseText(document.getText());
 		return new Promise((resolve, reject) => {
-			const r: number[][] = [];
-			const builder = new vscode.SemanticTokensBuilder();
+			if (tokenizeFlag === true) {
+				const r: number[][] = [];
+				const builder = new vscode.SemanticTokensBuilder();
 
-			kuromojiBuilder.build(async (err: any, tokenizer: any) => {
-				tokenCaching = true;
-				// 辞書がなかったりするとここでエラーになります(´・ω・｀)
-				if (err) {
-					console.dir('Kuromoji initialize error:' + err.message);
-					throw err;
-				}
-				//		for (let i = 0; i < lines.length; i++) {
-				let i = 0;
-				//const line = lines[i];
-
-				// tokenizer.tokenize に文字列を渡すと、その文を形態素解析してくれます。
-				const kuromojiToken = tokenizer.tokenize(document.getText());
-
-				console.dir(kuromojiToken);
-				let lineOffset = 0;
-				let openOffset = 0;
-				let closeOffset = 0;
-				let j = 0;
-
-				let isDialogue = false;
-				let isQuote = false;
-				let isMarkedProperNoun = false;
-				let isRuby = false;
-				for await (let mytoken of kuromojiToken) {
-					let nextToken = [];
-					mytoken = kuromojiToken[j];
-					if (j <= kuromojiToken.length - 1) {
-						nextToken = kuromojiToken[j + 1];
+				kuromojiBuilder.build(async (err: any, tokenizer: any) => {
+					tokenCaching = true;
+					// 辞書がなかったりするとここでエラーになります(´・ω・｀)
+					if (err) {
+						console.dir('Kuromoji initialize error:' + err.message);
+						throw err;
 					}
+					//		for (let i = 0; i < lines.length; i++) {
+					let i = 0;
+					//const line = lines[i];
 
-					let wordLength = 0;
-					wordLength = mytoken.surface_form.length;
+					// tokenizer.tokenize に文字列を渡すと、その文を形態素解析してくれます。
+					const kuromojiToken = tokenizer.tokenize(document.getText());
 
-					//改行処理
-					if (mytoken.surface_form.match(/\n/)) {
-						i += mytoken.surface_form.match(/\n/g).length;
-						//複数の改行が重なるとKuromojiは'\n\n'のように返す。
-						lineOffset = mytoken.word_position + mytoken.surface_form.length - 1;
-						openOffset = 0;
-						wordLength = 0;
-						isRuby = false;
-						isQuote = false;
-						//	console.log('line-feed:' + i + ": " + lineOffset);
-					} else {
-						openOffset = mytoken.word_position - lineOffset - 1;
-					}
+					console.dir(kuromojiToken);
+					let lineOffset = 0;
+					let openOffset = 0;
+					let closeOffset = 0;
+					let j = 0;
 
-					let tokenActivity = false;
-					let kind = mytoken.pos;
-					//console.log(mytoken.surface_form);
-					if (kind == '名詞') kind = 'noun';
-					if (mytoken.pos == '名詞' && mytoken.pos_detail_1 == '固有名詞') {
-						kind = 'proper_noun';
-						tokenActivity = true;
-					}
-					if (mytoken.pos == '名詞' && mytoken.pos_detail_1 == '代名詞') {
-						kind = 'pronoun';
-						tokenActivity = true;
-					}
-					if (mytoken.surface_form.match(/^(私|わたし|わたくし|我|われ|あたし|僕|ぼく|俺|おれ|貴方|あなた|あんた|お前|おまえ|君|きみ|てめえ|彼|かれ|彼女|彼女|あいつ|そいつ|こいつ|奴|やつ)$/) && mytoken.pos_detail_1 == '代名詞') {
-						kind = 'personal_pronoun';
-						tokenActivity = true;
-					}
-					if (kind == '記号') kind = 'punctuation'; tokenActivity = true;
+					let isDialogue = false;
+					let isQuote = false;
+					let isMarkedProperNoun = false;
+					let isRuby = false;
+					for await (let mytoken of kuromojiToken) {
+						let nextToken = [];
+						mytoken = kuromojiToken[j];
+						if (j <= kuromojiToken.length - 1) {
+							nextToken = kuromojiToken[j + 1];
+						}
 
-					if (mytoken.pos_detail_1.match(/括弧./) || mytoken.surface_form.match(/(》|［＃「)/)) {
-						kind = 'bracket'; tokenActivity = true;
-					}
+						let wordLength = 0;
+						wordLength = mytoken.surface_form.length;
 
-					if (kind == '動詞') kind = 'verb'; tokenActivity = true;
-					if (mytoken.pos_detail_1 == '数') kind = 'enum'; tokenActivity = true;
-					if (kind == 'noun' && mytoken.pos_detail_1 == '接尾') kind = 'suffix'; tokenActivity = true;
+						//改行処理
+						if (mytoken.surface_form.match(/\n/)) {
+							i += mytoken.surface_form.match(/\n/g).length;
+							//複数の改行が重なるとKuromojiは'\n\n'のように返す。
+							lineOffset = mytoken.word_position + mytoken.surface_form.length - 1;
+							openOffset = 0;
+							wordLength = 0;
+							isRuby = false;
+							isQuote = false;
+							//	console.log('line-feed:' + i + ": " + lineOffset);
+						} else {
+							openOffset = mytoken.word_position - lineOffset - 1;
+						}
 
-					if (kind == '助動詞') kind = 'auailiary_verb'; tokenActivity = true;
-					if (kind == '助詞') {
-						kind = 'particle'
-						tokenActivity = true;
-					}
-					if (kind == '副詞') kind = 'adverb'; tokenActivity = true;
-					if (kind == '感動詞') kind = 'interjection'; tokenActivity = true;
-					if (kind == '形容詞') kind = 'adjective'; tokenActivity = true;
+						let tokenActivity = false;
+						let kind = mytoken.pos;
+						//console.log(mytoken.surface_form);
+						if (kind == '名詞') kind = 'noun';
+						if (mytoken.pos == '名詞' && mytoken.pos_detail_1 == '固有名詞') {
+							kind = 'proper_noun';
+							tokenActivity = true;
+						}
+						if (mytoken.pos == '名詞' && mytoken.pos_detail_1 == '代名詞') {
+							kind = 'pronoun';
+							tokenActivity = true;
+						}
+						if (mytoken.surface_form.match(/^(私|わたし|わたくし|我|われ|あたし|僕|ぼく|俺|おれ|貴方|あなた|あんた|お前|おまえ|君|きみ|てめえ|彼|かれ|彼女|彼女|あいつ|そいつ|こいつ|奴|やつ)$/) && mytoken.pos_detail_1 == '代名詞') {
+							kind = 'personal_pronoun';
+							tokenActivity = true;
+						}
+						if (kind == '記号') kind = 'punctuation'; tokenActivity = true;
 
-					if (kind == 'noun' && mytoken.pos_detail_1 == 'サ変接続' && nextToken.conjugated_type.match(/^サ変/)) {
-						kind = 'verb';
-					}
+						if (mytoken.pos_detail_1.match(/括弧./) || mytoken.surface_form.match(/(》|［＃「)/)) {
+							kind = 'bracket'; tokenActivity = true;
+						}
 
-					let tokenModifireType = '';
+						if (kind == '動詞') kind = 'verb'; tokenActivity = true;
+						if (mytoken.pos_detail_1 == '数') kind = 'enum'; tokenActivity = true;
+						if (kind == 'noun' && mytoken.pos_detail_1 == '接尾') kind = 'suffix'; tokenActivity = true;
 
-					//会話モディファイア
-					if (mytoken.surface_form == '「') {
-						isDialogue = true;
-					}
-					if (isDialogue == true) {
-						tokenModifireType = 'dialogue';
-					}
-					if (mytoken.surface_form == '」') isDialogue = false;
+						if (kind == '助動詞') kind = 'auailiary_verb'; tokenActivity = true;
+						if (kind == '助詞') {
+							kind = 'particle'
+							tokenActivity = true;
+						}
+						if (kind == '副詞') kind = 'adverb'; tokenActivity = true;
+						if (kind == '感動詞') kind = 'interjection'; tokenActivity = true;
+						if (kind == '形容詞') kind = 'adjective'; tokenActivity = true;
 
-					//引用モディファイア
-					if (mytoken.surface_form == '『') isQuote = true;
-					if (isQuote == true) {
-						tokenModifireType = 'quote';
-					}
-					if (mytoken.surface_form == '』') isQuote = false;
+						if (kind == 'noun' && mytoken.pos_detail_1 == 'サ変接続' && nextToken.conjugated_type.match(/^サ変/)) {
+							kind = 'verb';
+						}
 
-					//固有名詞モディファイア
-					if (mytoken.surface_form == '〈') isMarkedProperNoun = true;
-					if (isMarkedProperNoun == true) {
-						kind = 'proper_noun';
-					}
-					if (mytoken.surface_form == '〉') isMarkedProperNoun = false;
+						let tokenModifireType = '';
 
-					//ルビモディファイア
-					if (mytoken.surface_form == '《') isRuby = true;
-					if (isRuby == true) {
-						tokenModifireType = 'aozora';
-					}
-					if (mytoken.surface_form == '》') {
-						isRuby = false;
-						kind = 'bracket';
-					}
+						//会話モディファイア
+						if (mytoken.surface_form == '「') {
+							isDialogue = true;
+						}
+						if (isDialogue == true) {
+							tokenModifireType = 'dialogue';
+						}
+						if (mytoken.surface_form == '」') isDialogue = false;
 
-					//青空注記モディファイア
-					if (mytoken.surface_form == '［' || mytoken.surface_form == '［＃「') isRuby = true;
-					if (isRuby == true) {
-						tokenModifireType = 'aozora';
-					}
-					if (mytoken.surface_form == '］') {
-						isRuby = false;
-						kind = 'bracket';
-					}
+						//引用モディファイア
+						if (mytoken.surface_form == '『') isQuote = true;
+						if (isQuote == true) {
+							tokenModifireType = 'quote';
+						}
+						if (mytoken.surface_form == '』') isQuote = false;
 
-					closeOffset = openOffset + wordLength;
-					const tokenData = parseTextToken(document.getText().substring(openOffset, closeOffset));
-					const tokenModifierNum = encodeTokenModifiers([tokenModifireType]);
+						//固有名詞モディファイア
+						if (mytoken.surface_form == '〈') isMarkedProperNoun = true;
+						if (isMarkedProperNoun == true) {
+							kind = 'proper_noun';
+						}
+						if (mytoken.surface_form == '〉') isMarkedProperNoun = false;
 
-					if (tokenActivity == true) {
-						builder.push(i, openOffset, wordLength, encodeTokenType(kind), tokenModifierNum);
-						//	console.log(i + ':' + j + '/' + openOffset + ':' + mytoken.surface_form);
+						//ルビモディファイア
+						if (mytoken.surface_form == '《') isRuby = true;
+						if (isRuby == true) {
+							tokenModifireType = 'aozora';
+						}
+						if (mytoken.surface_form == '》') {
+							isRuby = false;
+							kind = 'bracket';
+						}
+
+						//青空注記モディファイア
+						if (mytoken.surface_form == '［' || mytoken.surface_form == '［＃「') isRuby = true;
+						if (isRuby == true) {
+							tokenModifireType = 'aozora';
+						}
+						if (mytoken.surface_form == '］') {
+							isRuby = false;
+							kind = 'bracket';
+						}
+
+						closeOffset = openOffset + wordLength;
+						const tokenData = parseTextToken(document.getText().substring(openOffset, closeOffset));
+						const tokenModifierNum = encodeTokenModifiers([tokenModifireType]);
+
+						if (tokenActivity == true) {
+							builder.push(i, openOffset, wordLength, encodeTokenType(kind), tokenModifierNum);
+							//	console.log(i + ':' + j + '/' + openOffset + ':' + mytoken.surface_form);
+						}
+						openOffset = closeOffset;
+						if (j == kuromojiToken.length - 1) {
+							resolve(builder.build());
+							//const builder = new vscode.SemanticTokensBuilder();
+							//return builder.build();
+						}
+						j++;
 					}
-					openOffset = closeOffset;
-					if (j == kuromojiToken.length - 1) {
-						resolve(builder.build());
-						//const builder = new vscode.SemanticTokensBuilder();
-						//return builder.build();
-					}
-					j++;
-				}
-			});
+				});
+			} else {
+				const builder = new vscode.SemanticTokensBuilder();
+				builder.push(0,0,0,0,0);
+				resolve(builder.build());
+			}
 		});
 	}
 
@@ -276,7 +288,7 @@ export class DocumentRangeSemanticTokensProvider implements vscode.DocumentRange
 	provideDocumentRangeSemanticTokens(document: vscode.TextDocument, range: vscode.Range, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SemanticTokens> {
 		throw new Error('Method not implemented.');
 	}
-	
+
 }
 
 function parseTextToken(text: string): { tokenType: string; tokenModifiers: string[]; } {
