c7e66d509ff0b698a89baa1c2b4db8a9cac99805
Taiyo Fujii <taiyo@taiyolab.com>
2022-10-13 17:21:26 +0900

    サーバー

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import * as cp from "child_process";
 import * as http from "http";
+import * as net from "net";
 import * as path from "path";
 import * as fs from "fs";
 import * as os from "os";
@@ -22,9 +23,47 @@ const output = vscode.window.createOutputChannel("Novel");
 //リソースとなるhtmlファイル
 //let html: Buffer;
 let documentRoot: vscode.Uri;
-const servicePort = 8080;
-let isServerRunning = false;
+let WebViewPanel = false;
+let servicePort = 8080;
 let previewRedrawing = false;
+
+emptyPort(function (port: number) {
+  servicePort = port;
+  // console.log('真の空きポート',port);
+});
+
+function emptyPort(callback: any) {
+  let port = 8080;
+
+  const socket = new net.Socket();
+  const server = new net.Server();
+
+  socket.on("error", function (e) {
+    console.log('try:', port);
+    server.on('listening', () => {
+      server.close();
+      console.log('ok:', port);
+      callback(port);
+    }).on('error', () => {
+      console.log('ng:', port);
+      loop();
+    }).listen(port, '127.0.0.1');
+  });
+
+  function loop() {
+    port = port + 2;
+    if (port >= 20000) {
+      callback(new Error("empty port not found"));
+      return;
+    }
+
+    socket.connect(port, "127.0.0.1", function () {
+      socket.destroy();
+      loop();
+    });
+  }
+  loop();
+}
 
 //コマンド登録
 export function activate(context: vscode.ExtensionContext): void {
@@ -38,10 +77,7 @@ export function activate(context: vscode.ExtensionContext): void {
     vscode.commands.registerCommand("Novel.export-pdf", exportpdf)
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "Novel.launch-preview-server",
-      previewserver
-    )
+    vscode.commands.registerCommand("Novel.launch-preview-server", launchserver)
   );
 
   context.subscriptions.push(
@@ -129,17 +165,10 @@ export function activate(context: vscode.ExtensionContext): void {
 }
 
 let latestEditor: vscode.TextEditor;
-function launchserver(
-  originEditor: OriginEditor,
-  shouldOpenWebViewPanel: boolean
-) {
-  if (isServerRunning) {
-    if (shouldOpenWebViewPanel) {
-      openWebViewPanel();
-    }
-    return;
-  }
-  isServerRunning = true;
+
+function launchserver(originEditor: vscode.TextEditor) {
+  //もしサーバーが動いていたらポートの番号をずらす
+  latestEditor = originEditor;
 
   //Webサーバの起動。ドキュメントルートはnode_modules/novel-writer/htdocsになる。
   const viewerServer = http.createServer(function (request, response) {
@@ -191,138 +220,122 @@ function launchserver(
     });
   });
 
-  viewerServer.on("listening", () => {
-    console.log("真の空きポート", servicePort);
+  viewerServer.listen(servicePort);
 
-    // Node Websockets Serverを起動する
-    const wsServer = websockets.Server;
-    // TODO: Should not assume servicePort + 1 is always available
-    const s = new wsServer({ port: servicePort + 1 });
+  // Node Websockets Serverを起動する
+  const wsServer = websockets.Server;
+  const s = new wsServer({ port: servicePort + 1 });
 
-    s.on("connection", (ws) => {
-      //console.log(previewvariables());
-      ws.on("message", (messageRaw, isBinary) => {
-        //const messageAsString = JSON.stringify(messageRaw);
-        const message = isBinary ? messageRaw : messageRaw.toString();
+  s.on("connection", (ws) => {
+    //console.log(previewvariables());
+    ws.on("message", (messageRaw, isBinary) => {
+      //const messageAsString = JSON.stringify(messageRaw);
+      const message = isBinary ? messageRaw : messageRaw.toString();
 
-        console.log("Received: " + message);
+      console.log("Received: " + message);
 
-        if (message == "hello") {
-          //通信確立
-          ws.send(JSON.stringify(getConfig()));
-          ws.send(editorText(originEditor));
-        } else if (message == "givemedata") {
-          // データ送信要求を受け取った時
-          console.log("sending body");
-          ws.send(editorText(originEditor));
-        } else if (message == "redrawFinished") {
-          // 再描画終了を受け取った時
-          previewRedrawing = false;
-        } else if (message == "giveMeObject") {
-          // メタデータ送信要求を受け取った時
-          const sendingObjects = draftsObject(draftRoot());
-          console.log("send:", sendingObjects);
-          ws.send(JSON.stringify(sendingObjects));
-        } else if (typeof message == "string" && message.match(/^jump/)) {
-          //行のタップを検知した時
-          //const originalEditor = vscode.window.activeTextEditor;
+      if (message == "hello") {
+        //通信確立
+        ws.send(JSON.stringify(getConfig()));
+        ws.send(editorText(originEditor));
+      } else if (message == "givemedata") {
+        // データ送信要求を受け取った時
+        console.log("sending body");
+        ws.send(editorText(originEditor));
+      } else if (message == "redrawFinished") {
+        // 再描画終了を受け取った時
+        previewRedrawing = false;
+      } else if (message == "giveMeObject") {
+        // メタデータ送信要求を受け取った時
+        const sendingObjects = draftsObject(draftRoot());
+        console.log("send:", sendingObjects);
+        ws.send(JSON.stringify(sendingObjects));
+      } else if (typeof message == "string" && message.match(/^jump/)) {
+        //行のタップを検知した時
+        //const originalEditor = vscode.window.activeTextEditor;
 
-          const targetLine = parseInt(message.split("-")[1]);
-          const range = latestEditor.document.lineAt(targetLine).range;
+        const targetLine = parseInt(message.split("-")[1]);
+        const range = latestEditor.document.lineAt(targetLine).range;
 
-          if (typeof range != "undefined") {
-            console.log("go to line!");
-            latestEditor.selection = new vscode.Selection(
-              range?.start,
-              range?.end
-            );
-            latestEditor.revealRange(
-              range,
-              vscode.TextEditorRevealType.InCenter
-            );
-          }
+        if (typeof range != "undefined") {
+          console.log("go to line!");
+          latestEditor.selection = new vscode.Selection(
+            range?.start,
+            range?.end
+          );
+          latestEditor.revealRange(range, vscode.TextEditorRevealType.InCenter);
         }
-      });
+      }
     });
+  });
 
-    vscode.workspace.onDidChangeTextDocument((e) => {
-      let _a;
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    let _a;
+    if (
+      e.document ==
+      ((_a = vscode.window.activeTextEditor) === null || _a === void 0
+        ? void 0
+        : _a.document)
+    ) {
+      const editor = vscode.window.activeTextEditor;
+      if (typeof editor != "undefined") {
+        latestEditor = editor;
+        console.log("editor changed!");
+      }
       if (
-        e.document ==
-        ((_a = vscode.window.activeTextEditor) === null || _a === void 0
-          ? void 0
-          : _a.document)
+        editor?.document.languageId == "novel" ||
+        editor?.document.languageId == "markdown" ||
+        editor?.document.languageId == "plaintext"
       ) {
-        const editor = vscode.window.activeTextEditor;
-        if (typeof editor != "undefined") {
-          latestEditor = editor;
-          console.log("editor changed!");
-        }
-        if (
-          editor?.document.languageId == "novel" ||
-          editor?.document.languageId == "markdown" ||
-          editor?.document.languageId == "plaintext"
-        ) {
-          publishWebsocketsDelay.presskey(s);
-        }
+        publishWebsocketsDelay.presskey(s);
       }
-    });
-
-    vscode.window.onDidChangeTextEditorSelection((e) => {
-      if (e.textEditor == vscode.window.activeTextEditor) {
-        const editor = vscode.window.activeTextEditor;
-        if (typeof editor != "undefined") {
-          latestEditor = editor;
-          console.log("editor changed!");
-        }
-        if (
-          editor?.document.languageId == "novel" ||
-          editor?.document.languageId == "markdown" ||
-          editor?.document.languageId == "plaintext"
-        ) {
-          publishWebsocketsDelay.presskey(s);
-        }
-      }
-    });
-
-    vscode.workspace.onDidChangeConfiguration(() => {
-      //設定変更
-      console.log("setting changed");
-      sendsettingwebsockets(s);
-    });
-
-    vscode.window.onDidChangeVisibleTextEditors((e) => {
-      //ウインドウの状態変更
-      //プレビューが閉じたかどうか
-      console.log("WindowState Changed:", e);
-    });
-
-    vscode.window.onDidChangeVisibleTextEditors((e) => {
-      //ウインドウの状態変更
-      //プレビューが閉じたかどうか
-      console.log("WindowState Changed:", e);
-    });
-
-    if (shouldOpenWebViewPanel) {
-      openWebViewPanel();
     }
   });
 
-  viewerServer.listen(servicePort, "0.0.0.0");
-}
+  vscode.window.onDidChangeTextEditorSelection((e) => {
+    if (e.textEditor == vscode.window.activeTextEditor) {
+      const editor = vscode.window.activeTextEditor;
+      if (typeof editor != "undefined") {
+        latestEditor = editor;
+        console.log("editor changed!");
+      }
+      if (
+        editor?.document.languageId == "novel" ||
+        editor?.document.languageId == "markdown" ||
+        editor?.document.languageId == "plaintext"
+      ) {
+        publishWebsocketsDelay.presskey(s);
+      }
+    }
+  });
 
-function openWebViewPanel() {
-  const serversHostname = os.hostname();
-  const panel = vscode.window.createWebviewPanel(
-    "preview", // Identifies the type of the webview. Used internally
-    "原稿プレビュー http://" + serversHostname + ":" + servicePort, // Title of the panel displayed to the user
-    vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-    {
-      enableScripts: true,
-    } // Webview options. More on these later.
-  );
+  vscode.workspace.onDidChangeConfiguration(() => {
+    //設定変更
+    console.log("setting changed");
+    sendsettingwebsockets(s);
+  });
 
-  panel.webview.html = `<!DOCTYPE html>
+  vscode.window.onDidChangeVisibleTextEditors((e) => {
+    //ウインドウの状態変更
+    //プレビューが閉じたかどうか
+    console.log("WindowState Changed:", e);
+  });
+
+  publishWebsocketsDelay.presskey(s);
+
+  if (WebViewPanel) {
+    //    vscode.window.showInformationMessage('Hello, world!');
+    const serversHostname = os.hostname();
+    const panel = vscode.window.createWebviewPanel(
+      "preview", // Identifies the type of the webview. Used internally
+      "原稿プレビュー http://" + serversHostname + ":" + servicePort, // Title of the panel displayed to the user
+      vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+      {
+        enableScripts: true,
+      } // Webview options. More on these later.
+    );
+
+    panel.webview.html = `<!DOCTYPE html>
   <html>
       <head>
           <style>
@@ -337,6 +350,7 @@ function openWebViewPanel() {
           <iframe src="http://localhost:${servicePort}" frameBorder="0" style="min-width: 100%; min-height: 100%" />
       </body>
   </html>`;
+  }
 }
 
 function publishwebsockets(socketServer: websockets.Server) {
@@ -355,8 +369,6 @@ function sendsettingwebsockets(socketServer: websockets.Server) {
 const publishWebsocketsDelay: any = {
   publish: function (socketServer: websockets.Server) {
     publishwebsockets(socketServer);
-    //keyPressFlag = false;
-    //delete this.timeoutID;
   },
   presskey: function (s: websockets.Server) {
     if (previewRedrawing) return;
@@ -365,14 +377,12 @@ const publishWebsocketsDelay: any = {
   },
 };
 
-function previewserver() {
-  const originEditor = vscode.window.activeTextEditor;
-  launchserver(originEditor, false);
-}
-
 function verticalpreview() {
   const originEditor = vscode.window.activeTextEditor;
-  launchserver(originEditor, true);
+  WebViewPanel = true;
+  if (typeof originEditor != "undefined") {
+    launchserver(originEditor);
+  }
 }
 
 function exportpdf(): void {
@@ -571,7 +581,7 @@ function getPrintContent() {
       p {
         font-size: ${fontSizeWithUnit};
         line-height: 1.75;
-        height: calc(${fontSizeWithUnit} * ${previewSettings.lineLength} + 0.05rem);
+        height: calc(${fontSizeWithUnit} * ${previewSettings.lineLength} + 0.5mm);
         text-indent: 0em;
         hanging-punctuation: force-end;
         line-break:strict;
