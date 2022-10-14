c393e40ca5df312ea5403d288ff1a834fdf97ab4
Taiyo Fujii <taiyo@taiyolab.com>
2022-10-14 11:50:49 +0900

    debug Title of PDF preview

--- src/extension.ts
+++ src/extension.ts
@@ -459,7 +459,7 @@ function getPrintContent() {
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Cat Coding</title>
+      <title>${projectTitle}</title>
 
       <style>
       @charset "UTF-8";
