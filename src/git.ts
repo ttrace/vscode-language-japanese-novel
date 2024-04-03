0bb073a3d314128068a87ef46bd7425df77734ec
Taiyo Fujii <taiyo@taiyolab.com>
2024-04-03 12:01:54 +0900

    basic markdown

--- src/git.ts
+++ src/git.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import simpleGit, { SimpleGit } from "simple-git";
-import { CharacterCounter, CharacterCounterController } from "./charactorcount";
+// import { CharacterCounter, CharacterCounterController } from "./charactorcount";
 
 //instruction: from https://github.com/steveukx/git-js#readme
 
@@ -17,45 +17,39 @@ export class NovelGit {
       return false;
     }
   }
-
-  public _getDayBackString(filePath: string): any {
-    // https://qiita.com/nju33/items/3905105900e7ae726d19
-    // Gitで過去のある時点のファイルの内容を見る
-    // git show `git log -1 --format='%h' --before=midnight`:package.json
-    //let result = '';
-    const relatevePath = path.relative(this.projectPath, filePath);
-
-    const git: SimpleGit = simpleGit(this.projectPath);
-    let latestHash = "";
-
-    const logOption = { file: relatevePath, "--before": "yesterday", n: 1 };
-    let showString = "";
-    git
-      .log(logOption)
-      .catch((err) => {
-        console.error("failed:", err);
-        // vscode.window.showInformationMessage(`昨日以前に書かれた原稿がGitにコミットされていないようです`);
-      })
-      .then((logs: any) => {
-        //console.log(logs);
-        if (logs.total === 0) {
-          //  vscode.window.showInformationMessage(`昨日以前に書かれた原稿がGitにコミットされていないようです`);
-        } else {
-          latestHash = logs.all[0].hash;
-          showString = latestHash + ":" + relatevePath;
-          //console.log('showString: ',showString);
-          git
-            .show(showString)
-            .catch((err) => console.error("failed to git show:", err))
-            .then((showLog) => {
-              const counter = new CharacterCounter();
-              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              if (typeof showLog == "string") {
-                counter._setLatestUpdate(showLog);
-                //console.log('log of Show: ',showLog);
-              }
-            });
-        }
+  
+    // コンストラクタ等その他の実装は省略
+  
+    public _getDayBackString(filePath: string): Promise<string> {
+      const relatevePath = path.relative(this.projectPath, filePath);
+      const git: SimpleGit = simpleGit(this.projectPath);
+  
+      const logOption = { file: relatevePath, "--before": "yesterday", n: 1 };
+  
+      return new Promise((resolve, reject) => {
+        let showString = "";
+        git.log(logOption)
+          .then((logs) => {
+            if (logs.total === 0) {
+              resolve(""); // コミットがない場合は空文字列を返します
+            } else {
+              const latestHash = logs.latest?.hash;
+              showString = `${latestHash}:${relatevePath}`;
+              return git.show(showString);
+            }
+          })
+          .then((showLog) => {
+            if (typeof showLog === "string") {
+              // 文字列を処理し、必要な結果を取得した後、それを解決します。
+              resolve(showLog);
+            }
+          })
+          .catch((err) => {
+            console.error("failed:", err);
+            reject(err);
+          });
       });
-  }
+    }
+  
+  
 }
